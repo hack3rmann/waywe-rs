4665f254cd19d61ad52b9992f930d0479bf81d48
Svyatoslav <slava-varov3@mail.ru>
2025-02-18 17:55:49 +0300

    add `MessageBuilder` to wire

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -2,7 +2,7 @@ pub mod wayland;
 
 use std::collections::HashMap;
 use std::{env, error::Error, io::Write, os::unix::net::UnixStream};
-use wayland::wire::{self, Message, MessageReader};
+use wayland::wire::{self, Message, MessageBuilder, MessageReader};
 
 fn get_socket_path() -> Option<String> {
     let xdg_runtime_dir = env::var("XDG_RUNTIME_DIR").ok()?;
@@ -21,9 +21,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     let socket_path = get_socket_path().expect("failed to get wayland socket path");
     let mut sock = UnixStream::connect(socket_path)?;
 
-    sock.write_all(bytemuck::cast_slice(&[0x00000001, 0x000C0001, 0x00000002]))?;
-
     let mut buf = Vec::new();
+    let mut message_builder = MessageBuilder::new(&mut buf);
+
+    let message = message_builder
+        .object_id(1)
+        .request_id(1)
+        .uint(2)
+        .get_message()
+        .unwrap();
+
+    sock.write_all(message.as_bytes())?;
+
     let mut registry = HashMap::<String, InterfaceDesc>::new();
 
     for _ in 0..53 {
@@ -42,16 +51,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         );
     }
 
-    eprintln!("had read entire registry");
-
-    let compositor_id = registry["wl_compositor"].object_id;
-
-    sock.write_all(bytemuck::cast_slice(&[compositor_id, 0x000C0001, 0xFF000000]))?;
-
-    wire::read_message_into(&mut sock, &mut buf)?;
-    let message = Message::from_u32_slice(&buf);
-
-    dbg!(message.header());
+    dbg!(registry);
 
     Ok(())
 }
