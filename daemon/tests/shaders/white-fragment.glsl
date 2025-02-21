d336dac3dcafab8d3576ee5e6a3fe6ca0250d146
Svyatoslav <slava-varov3@mail.ru>
2025-02-21 13:47:28 +0300

    add triangle render pipeline to wgpu test

---
+++ daemon/tests/shaders/white-fragment.glsl
@@ -0,0 +1,7 @@
+#version 460
+
+out vec4 surface_color;
+
+void main() {
+    surface_color = vec4(1.0);
+}
--- daemon/tests/white-rect.rs
+++ daemon/tests/white-rect.rs
@@ -1,4 +1,7 @@
+use std::mem;
+
 use wayland::init::WaylandContext;
+use wgpu::util::DeviceExt as _;
 
 #[tokio::test]
 async fn t1() -> Result<(), Box<dyn std::error::Error>> {
@@ -41,7 +44,16 @@ async fn t1() -> Result<(), Box<dyn std::error::Error>> {
         )
         .await?;
 
-    let _vertex_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+    const WIDTH: u32 = 1000;
+    const HEIGHT: u32 = WIDTH;
+    surface.configure(
+        &device,
+        &surface.get_default_config(&adapter, WIDTH, HEIGHT).unwrap(),
+    );
+
+    let surface_format = surface.get_capabilities(&adapter).formats[0];
+
+    let vertex_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Glsl {
             shader: include_str!("shaders/white-vertex.glsl").into(),
@@ -50,5 +62,105 @@ async fn t1() -> Result<(), Box<dyn std::error::Error>> {
         },
     });
 
-    Ok(())
+    let fragment_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Glsl {
+            shader: include_str!("shaders/white-fragment.glsl").into(),
+            stage: wgpu::naga::ShaderStage::Fragment,
+            defines: Default::default(),
+        },
+    });
+
+    let triangle: [[f32; 2]; 3] = [[-0.5, -0.5], [0.5, -0.5], [0.0, 0.5]];
+
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::bytes_of(&triangle),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &vertex_shader,
+            entry_point: Some("main"),
+            compilation_options: wgpu::PipelineCompilationOptions {
+                constants: &Default::default(),
+                zero_initialize_workgroup_memory: false,
+            },
+            buffers: &[wgpu::VertexBufferLayout {
+                array_stride: mem::size_of_val(&triangle[0]) as u64,
+                step_mode: wgpu::VertexStepMode::Vertex,
+                attributes: &[wgpu::VertexAttribute {
+                    format: wgpu::VertexFormat::Float32x2,
+                    offset: 0,
+                    shader_location: 0,
+                }],
+            }],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &fragment_shader,
+            entry_point: Some("main"),
+            compilation_options: wgpu::PipelineCompilationOptions {
+                constants: &Default::default(),
+                zero_initialize_workgroup_memory: false,
+            },
+            targets: &[Some(wgpu::ColorTargetState {
+                // FIXME(hack3rmann): figure out wayland's texture format
+                format: surface_format,
+                blend: None,
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: None,
+            unclipped_depth: false,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
+        cache: None,
+    });
+
+    let surface_texture = surface.get_current_texture()?;
+    let surface_view = surface_texture.texture.create_view(&Default::default());
+
+    let mut encoder = device.create_command_encoder(&Default::default());
+
+    loop {
+        let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &surface_view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+
+        pass.set_pipeline(&pipeline);
+        pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+        pass.draw(0..triangle.len() as u32, 0..1);
+    }
 }
