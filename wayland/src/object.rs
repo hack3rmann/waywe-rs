8881315e57d9d47eaa8fde7c9cfbf8d50796e7a8
Svyatoslav <slava-varov3@mail.ru>
2025-02-27 21:25:33 +0300

    remove `IdProvider` and `ObjectIdMap`

--- wayland/src/object.rs
+++ wayland/src/object.rs
@@ -1,5 +1,4 @@
 use std::num::NonZeroU32;
-use fxhash::FxHashMap;
 use thiserror::Error;
 
 /// Ids and names for Wayland objects
@@ -55,83 +54,3 @@ impl TryFrom<u32> for ObjectId {
 #[derive(Debug, Error)]
 #[error("invalid zero `ObjectId`")]
 pub struct ZeroObjectIdError;
-
-/// Unique identifier provider for Wayland objects
-#[derive(Clone, Debug, PartialEq, Default, Eq, Copy, PartialOrd, Ord, Hash)]
-pub struct ObjectIdProvider {
-    pub last: ObjectId,
-}
-
-impl ObjectIdProvider {
-    /// Creates new [`ObjectIdProvider`].
-    pub const fn new(last_used: ObjectId) -> Self {
-        Self {
-            last: ObjectId::new(last_used.0.get()),
-        }
-    }
-
-    /// Gives the next available id. Basically, `prev_id + 1`
-    pub const fn next_id(&mut self) -> ObjectId {
-        let result = self.last;
-        self.last.0 = NonZeroU32::new(self.last.0.get().wrapping_add(1)).unwrap();
-        result
-    }
-
-    pub fn from_mapped_ids(map: &ObjectIdMap) -> Self {
-        let max_id = map.iter().map(|(&_name, &id)| id).max().unwrap();
-        Self::new(max_id)
-    }
-}
-
-/// A bijective map that maps object names to their ids
-#[derive(Clone, Debug, PartialEq, Default, Eq)]
-pub struct ObjectIdMap {
-    pub(crate) name_to_id: FxHashMap<ObjectId, ObjectId>,
-    pub(crate) id_to_name: FxHashMap<ObjectId, ObjectId>,
-}
-
-impl ObjectIdMap {
-    /// Constructs new [`ObjectIdMap`]
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    /// Returns object id by the object name
-    pub fn get_id(&self, name: ObjectId) -> Option<ObjectId> {
-        self.name_to_id.get(&name).copied()
-    }
-
-    /// Returns object name by the object id
-    pub fn get_name(&self, id: ObjectId) -> Option<ObjectId> {
-        self.id_to_name.get(&id).copied()
-    }
-
-    /// Inserts new name-id pair
-    pub fn map(&mut self, name: ObjectId, id: ObjectId) {
-        self.name_to_id.insert(name, id);
-        self.id_to_name.insert(id, name);
-    }
-
-    /// Returns an iterator over `(name, id)` pairs
-    pub fn iter(&self) -> <&'_ FxHashMap<ObjectId, ObjectId> as IntoIterator>::IntoIter {
-        self.name_to_id.iter()
-    }
-}
-
-impl IntoIterator for ObjectIdMap {
-    type Item = (ObjectId, ObjectId);
-    type IntoIter = <FxHashMap<ObjectId, ObjectId> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.name_to_id.into_iter()
-    }
-}
-
-impl<'s> IntoIterator for &'s ObjectIdMap {
-    type Item = (&'s ObjectId, &'s ObjectId);
-    type IntoIter = <&'s FxHashMap<ObjectId, ObjectId> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.name_to_id.iter()
-    }
-}
