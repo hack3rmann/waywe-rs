b95dcf5d71692255ec43b4d3acf20af354677763
Svyatoslav <slava-varov3@mail.ru>
2025-03-11 15:18:15 +0300

    add wayland-scanner as a dependency

--- wayland-scanner/src/lib.rs
+++ wayland-scanner/src/lib.rs
@@ -2,8 +2,10 @@ mod xml;
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
 use std::{fs, path::PathBuf};
 use syn::{LitStr, parse2};
+use xml::ProtocolFile;
 
 #[proc_macro]
 pub fn include_wl_interfaces(token_stream: TokenStream) -> TokenStream {
@@ -13,8 +15,11 @@ pub fn include_wl_interfaces(token_stream: TokenStream) -> TokenStream {
 fn include_wl_interfaces_impl(token_stream: TokenStream2) -> TokenStream2 {
     let str_lit = parse2::<LitStr>(token_stream).expect("expecting string literal");
     let path = PathBuf::from(str_lit.value());
-    let _file_contents = fs::read_to_string(&path)
+    let file_contents = fs::read_to_string(&path)
         .unwrap_or_else(|err| panic!("failed to read file '{}': {err}", path.display()));
 
-    todo!()
+    let _protocol_file = xml_serde::from_str::<ProtocolFile>(&file_contents)
+        .unwrap_or_else(|err| panic!("failed to parse protocol from {}: {err}", path.display()));
+
+    quote! {}
 }
--- wayland/src/lib.rs
+++ wayland/src/lib.rs
@@ -2,3 +2,7 @@ pub mod init;
 pub mod interface;
 pub mod object;
 pub mod sys;
+
+use wayland_scanner::include_wl_interfaces;
+
+include_wl_interfaces!("wayland-protocols/wayland.xml");
