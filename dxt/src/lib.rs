2ed5ffa1f90bd47a53aa52bd9f478a0e8b56fef0
ArnoDarkrose <afilimonov533@gmail.com>
2025-03-11 17:18:44 +0300

    add dxt decompression

---
+++ dxt/src/lib.rs
@@ -0,0 +1,236 @@
+use safe_transmute::{guard::SingleManyGuard, transmute_many, transmute_one_to_bytes};
+
+pub enum DxtFormat {
+    Dxt1,
+    Dxt3,
+    Dxt5,
+}
+
+pub fn decompress_image(width: usize, height: usize, data: &[u32], format: DxtFormat) -> Vec<u32> {
+    let block_size = match format {
+        DxtFormat::Dxt1 => 2,
+        DxtFormat::Dxt3 | DxtFormat::Dxt5 => 4,
+    };
+
+    let num_blocks_width = (width + 3) / 4;
+    let num_blocks_height = (height + 3) / 4;
+    let expected_data_size = num_blocks_height * num_blocks_width * block_size;
+
+    assert!(
+        data.len() >= expected_data_size,
+        "corrupt data in decompression, byte array length mismatched"
+    );
+
+    let mut res = vec![0; width * height];
+
+    let mut block_idx = 0;
+    for block_y in 0..num_blocks_height {
+        for block_x in 0..num_blocks_width {
+            let offset = block_idx * block_size;
+            let data = &data[offset..offset + block_size];
+
+            let x = block_x * 4;
+            let y = block_y * 4;
+
+            match format {
+                DxtFormat::Dxt1 => decompress_dxt_1_block(data, &mut res, x, y, width, height),
+                DxtFormat::Dxt3 => decompress_dxt_3_block(data, &mut res, x, y, width, height),
+                DxtFormat::Dxt5 => decompress_dxt_5_block(data, &mut res, x, y, width, height),
+            }
+
+            block_idx += 1;
+        }
+    }
+
+    res
+}
+
+#[rustfmt::skip]
+fn get_color_palette(data: u32, format: DxtFormat) -> [u32; 4] {
+    // TODO(ArnoDarkrose): handle error
+
+    let colors = transmute_many::<u16, SingleManyGuard>(transmute_one_to_bytes(&data))
+        .expect("failed to transmute u32 to u16 slice");
+
+    let color0 = colors[0] as u32;
+    let color1 = colors[1] as u32;
+
+    // Convert RGB565 to RGB888
+    let r0 = (((color0 >> 11) & 31) * 255 / 31) as u8;
+    let g0 = (((color0 >> 5) & 63) * 255 / 63) as u8;
+    let b0 = (((color0 & 31) * 255) / 31) as u8;
+
+    let r1 = (((color1 >> 11) & 31) * 255 / 31) as u8;
+    let g1 = (((color1 >> 5) & 63) * 255 / 63) as u8;
+    let b1 = ((color1 & 31) * 255 / 31) as u8;
+
+    // NOTE(ArnoDarkrose): maybe make this from_be_bytes
+    let mut palette: [u32; 4] = [
+        u32::from_le_bytes([r0, g0, b0, 255]),
+        u32::from_le_bytes([r1, g1, b1, 255]),
+        0,
+        0,
+    ];
+
+    if color0 > color1 || matches!(format, DxtFormat::Dxt3 | DxtFormat::Dxt5) {
+        palette[2] =
+            u32::from_le_bytes([
+                (2 * r0 + r1) / 3,
+                (2 * g0 + g1) / 3,
+                (2 * b0 + b1) / 3,
+                255
+            ]);
+
+        palette[3] =
+            u32::from_le_bytes([
+                (r0 + 2 * r1) / 3,
+                (g0 + 2 * g1) / 3,
+                (b0 + 2 * b1) / 3,
+                255
+            ]);
+
+    } else {
+        palette[2] = u32::from_le_bytes([
+            (r0 + r1) / 2,
+            (g0 + g1) / 2,
+            (b0 + b1) / 2,
+            255
+        ]);
+    }
+
+    palette
+}
+
+fn decompress_dxt_1_block(
+    data: &[u32],
+    pixels: &mut [u32],
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+) {
+    let palette = get_color_palette(data[0], DxtFormat::Dxt1);
+
+    let byte_view = transmute_one_to_bytes(&data[1]);
+    for i in 0..4 {
+        for j in 0..4 {
+            if y + i < height && x + j < width {
+                // Each pixel index (in block) takes 2 bits, so one byte can encode 4 indices
+                let color_idx = (byte_view[i] >> (j * 2)) & 3;
+                let color = palette[color_idx as usize];
+
+                pixels[(y + i) * width + x + j] = color;
+            }
+        }
+    }
+}
+
+fn decompress_dxt_3_block(
+    data: &[u32],
+    pixels: &mut [u32],
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+) {
+    let first_num_byte_view = transmute_one_to_bytes(&data[0]);
+    let second_num_byte_view = transmute_one_to_bytes(&data[1]);
+
+    let color_palette = get_color_palette(data[2], DxtFormat::Dxt3);
+
+    let color_indices_byte_view = transmute_one_to_bytes(&data[3]);
+    for i in 0..4 {
+        for j in 0..4 {
+            if y + i < height && x + j < width {
+                // Each pixel index (in block) takes 2 bits, so one byte can encode 4 indices
+                let color_idx = (color_indices_byte_view[i] >> (j * 2)) & 3;
+                let mut color = color_palette[color_idx as usize];
+
+                let idx = i * 4 + j;
+
+                let byte = if idx < 8 {
+                    first_num_byte_view[idx]
+                } else {
+                    second_num_byte_view[idx % 8]
+                };
+
+                let alpha = if idx % 2 == 0 {
+                    (byte & 0xF) * 17
+                } else {
+                    (byte >> 4) * 17
+                };
+
+                color &= u32::MAX << 8;
+                color |= alpha as u32;
+
+                pixels[(y + i) * width + x + j] = color;
+            }
+        }
+    }
+}
+
+fn decompress_dxt_5_block(
+    data: &[u32],
+    pixels: &mut [u32],
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+) {
+    let first_num_bytes_view = transmute_one_to_bytes(&data[0]);
+    let second_num_bytes_view = transmute_one_to_bytes(&data[1]);
+
+    let alpha0 = first_num_bytes_view[0];
+    let alpha1 = first_num_bytes_view[1];
+
+    let mut alpha_palette = [alpha0, alpha1, 0, 0, 0, 0, 0, 0];
+
+    if alpha0 > alpha1 {
+        for i in 0u8..6 {
+            alpha_palette[(i + 2) as usize] = ((6 - i) * alpha0 + (i + 1) * alpha1) / 7;
+        }
+    } else {
+        for i in 0_u8..4 {
+            alpha_palette[(i + 2) as usize] = ((4 - i) * alpha0 + (i + 1) * alpha1) / 5;
+        }
+
+        alpha_palette[7] = 255;
+    }
+
+    let color_palette = get_color_palette(data[2], DxtFormat::Dxt5);
+
+    let mut bits = 0;
+    let mut cur_byte = 0;
+    let color_indices_byte_view = transmute_one_to_bytes(&data[3]);
+    for i in 0..4 {
+        for j in 0..4 {
+            if bits < 3 {
+                let idx = 2 + (i * 4 + j) * 3 / 8;
+                if idx < 8 {
+                    cur_byte |= first_num_bytes_view[idx] << bits;
+                } else {
+                    cur_byte |= second_num_bytes_view[idx % 8] << bits;
+                }
+                bits += 8;
+            }
+
+            let alpha_idx = cur_byte & 7;
+
+            if y + i < height && x + j < width {
+                // Each pixel index (in block) takes 2 bits, so one byte can encode 4 indices
+                let color_idx = (color_indices_byte_view[i] >> (j * 2)) & 3;
+                let mut color = color_palette[color_idx as usize];
+
+                let alpha = alpha_palette[alpha_idx as usize];
+
+                color &= u32::MAX << 8;
+                color |= alpha as u32;
+
+                pixels[(y + i) * width + x + j] = color;
+            }
+
+            cur_byte >>= 3;
+            bits -= 3;
+        }
+    }
+}
