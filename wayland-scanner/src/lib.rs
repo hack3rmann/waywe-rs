813c9f421f853b674b852d6c83a7f9591f6e6b58
Svyatoslav <slava-varov3@mail.ru>
2025-03-15 07:34:21 +0300

    add better example in doc-comment for wayland-scanner's `include_wl_interfaces` macro

--- wayland-scanner/src/lib.rs
+++ wayland-scanner/src/lib.rs
@@ -9,9 +9,20 @@ use proc_macro::TokenStream;
 /// # Example
 ///
 /// ```rust
+/// extern crate wayland_sys;
+///
 /// mod protocols {
 ///     wayland_scanner::include_wl_interfaces!("wayland-protocols/wayland.xml");
 /// }
+///
+/// # fn _asserts() {
+/// assert_eq!(protocols::wl_display::INTERFACE.name, c"wl_display");
+/// assert_eq!(protocols::wl_display::INTERFACE.version.get(), 1);
+///
+/// assert!(protocols::wl_surface::INTERFACE.version.get() >= 1);
+/// assert_eq!(protocols::wl_surface::INTERFACE.methods[1].name, c"attach");
+/// assert_eq!(protocols::wl_surface::INTERFACE.methods[1].signature, c"?oii");
+/// # }
 /// ```
 #[proc_macro]
 pub fn include_wl_interfaces(token_stream: TokenStream) -> TokenStream {
