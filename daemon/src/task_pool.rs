c66a919e58b4529d39eaeea0d56ec72689baa6a4
Svyatoslav <slava-varov3@mail.ru>
2025-06-23 02:27:47 +0300

    remove unused tasks

--- daemon/src/task_pool.rs
+++ daemon/src/task_pool.rs
@@ -1,5 +1,5 @@
 use crate::event::EventEmitter;
-use std::thread::JoinHandle;
+use std::{panic, thread::JoinHandle};
 
 pub struct TaskPool<T> {
     pub handles: Vec<JoinHandle<()>>,
@@ -14,13 +14,36 @@ impl<T> TaskPool<T> {
         }
     }
 
+    pub fn erase_finished(&mut self) -> usize {
+        let n_finished = 0;
+
+        for i in 0..self.handles.len() {
+            if i + n_finished >= self.handles.len() {
+                break;
+            }
+
+            if self.handles[i].is_finished() {
+                let handle = self.handles.swap_remove(i);
+
+                if let Err(payload) = handle.join() {
+                    panic::resume_unwind(payload);
+                }
+            }
+        }
+
+        n_finished
+    }
+
     pub fn spawn<F>(&mut self, f: F)
     where
         F: FnOnce(EventEmitter<T>) + Send + 'static,
         T: Send + 'static,
     {
+        self.erase_finished();
+
         let emitter = self.emitter.clone();
         let handle = std::thread::spawn(move || f(emitter));
+
         self.handles.push(handle);
     }
 }
