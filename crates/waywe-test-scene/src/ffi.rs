022b3e78f11257211e0d7f88cf1d14c0865f8d04
hack3rmann <slava-varov3@mail.ru>
2026-06-07 14:09:24 +0300

    extract create_opaque_renderer

--- crates/waywe-test-scene/src/ffi.rs
+++ crates/waywe-test-scene/src/ffi.rs
@@ -14,29 +14,31 @@ use waywe_subrenderer::{
     ffi::PanicPayload,
 };
 
+fn create_opaque_renderer(desc: &OpaqueRendererDesc) -> OpaqueRenderer {
+    let gpu = Arc::new(pollster::block_on(Gpu::new()));
+    let monitor = Monitor {
+        size: UVec2::new(
+            desc.surface_desc.extent.width,
+            desc.surface_desc.extent.height,
+        ),
+        surface_format: desc.surface_desc.wgpu_format,
+    };
+
+    let mut wallpaper = Wallpaper::new(Arc::clone(&gpu), monitor);
+    SceneTestWallpaper.build(&mut wallpaper);
+
+    OpaqueRenderer::new(SceneRenderer {
+        gpu,
+        scene: PreparedWallpaper::prepare(wallpaper),
+    })
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn waywe_ffi_create_opaque_renderer(
     desc: &OpaqueRendererDesc,
     out_renderer: &mut MaybeUninit<OpaqueRenderer>,
 ) -> PanicPayload {
-    let result = panic::catch_unwind(move || {
-        let gpu = Arc::new(pollster::block_on(Gpu::new()));
-        let monitor = Monitor {
-            size: UVec2::new(
-                desc.surface_desc.extent.width,
-                desc.surface_desc.extent.height,
-            ),
-            surface_format: desc.surface_desc.wgpu_format,
-        };
-
-        let mut wallpaper = Wallpaper::new(Arc::clone(&gpu), monitor);
-        SceneTestWallpaper.build(&mut wallpaper);
-
-        OpaqueRenderer::new(SceneRenderer {
-            gpu,
-            scene: PreparedWallpaper::prepare(wallpaper),
-        })
-    });
+    let result = panic::catch_unwind(move || create_opaque_renderer(desc));
 
     PanicPayload::map(result, |renderer| {
         out_renderer.write(renderer);
