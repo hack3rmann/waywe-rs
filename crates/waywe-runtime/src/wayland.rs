77fdb14d24d5c71b3ed6e2c9e527cb356ad70596
hack3rmann <slava-varov3@mail.ru>
2026-06-10 09:58:50 +0300

    remove wrong todo

--- crates/waywe-runtime/src/wayland.rs
+++ crates/waywe-runtime/src/wayland.rs
@@ -266,7 +266,6 @@ impl Dispatch for LayerSurface {
             let mut monitors = state.monitors.write().unwrap();
             let monitor = monitors.get_mut(&self.monitor_id).unwrap();
 
-            // TODO(hack3rmann): skip the first configure-resize
             // this is resize if and only if monitor is ininialized
             // and size is changed indeed
             match monitor.size {
